6798e2cef0af52edaa817cf2422208abf7d56a9e
VPhuong2K6 <nvphuong2k6@gmail.com>
2025-10-02 23:03:03 +0700

    Update Part2 Ex04MaxNumber

---
+++ src/Part2ConditionalStatements/Ex04MaxNumber.java
@@ -0,0 +1,19 @@
+package Part2.conditional.statements;
+import java.util.Scanner;
+public class Ex04MaxNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập số thứ nhất: ");
+        int a = sc.nextInt();
+        System.out.print("Nhập số thứ hai: ");
+        int b = sc.nextInt();
+
+        if (a > b) {
+            System.out.println("Số lớn hơn là: " + a);
+        } else if(b > a){
+            System.out.println( "Số lớn hơn là: " + b);
+        } else {
+            System.out.println("Hai số bằng nhau");
+        }
+    }
+}
