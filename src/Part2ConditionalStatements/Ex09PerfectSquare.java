e1e52e1ef7b21028bf9a91d9afa71032cbd46348
VPhuong2K6 <nvphuong2k6@gmail.com>
2025-10-04 09:13:08 +0700

    Update Part2 Ex09PerfectSquare

---
+++ src/Part2ConditionalStatements/Ex09PerfectSquare.java
@@ -0,0 +1,21 @@
+package part2.conditional.statements;
+
+import java.util.Scanner;
+public class Ex09PerfectSquare {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập số nguyên dương N: ");
+        int N = sc.nextInt();
+
+        if (N <= 0) {
+            System.out.println("Vui lòng nhập số nguyên dương lớn hơn 0");
+        } else {
+            int can = (int) Math.sqrt(N);
+            if (can * can == N) {
+                System.out.println(N + " là số chính phương");
+            } else {
+                System.out.println(N + " không phải là số chính phương");
+            }
+        }
+    }
+}
