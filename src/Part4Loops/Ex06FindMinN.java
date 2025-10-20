57b60e67cda5e8e7943cbe3e526080e636786598
VPhuong2K6 <nvphuong2k6@gmail.com>
2025-10-20 13:37:38 +0700

    Update Part4 Ex06FindMinN

---
+++ src/Part4Loops/Ex06FindMinN.java
@@ -0,0 +1,16 @@
+package Part4Loops;
+import java.util.Scanner;
+
+public class Ex06FindMinN {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập S: ");
+        int S = sc.nextInt();
+        int sum = 0, N = 0;
+        while (sum + N + 1 <= S) {
+            N++;
+            sum += N;
+        }
+        System.out.println("N nhỏ nhất = " + N);
+    }
+}
