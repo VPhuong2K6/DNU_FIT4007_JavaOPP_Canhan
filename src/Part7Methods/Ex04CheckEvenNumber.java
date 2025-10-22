b6b37bb059dc6f9b67d89377c1b2b888a773d029
VPhuong2K6 <nvphuong2k6@gmail.com>
2025-10-22 22:05:48 +0700

    Update Part7 Ex04CheckEvenNumber

---
+++ src/Part7Methods/Ex04CheckEvenNumber.java
@@ -0,0 +1,11 @@
+package Part7Methods;
+
+public class Ex04CheckEvenNumber {
+    static boolean isEven(int n) {
+        return n % 2 == 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isEven(6));
+    }
+}
