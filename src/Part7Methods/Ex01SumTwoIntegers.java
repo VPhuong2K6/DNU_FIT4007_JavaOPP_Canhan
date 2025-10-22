2fa0d33e7213dfc78b8f655ce375b1b4203c3f80
VPhuong2K6 <nvphuong2k6@gmail.com>
2025-10-22 21:13:46 +0700

    Update Part7 Ex01SumTwoIntegers

---
+++ src/Part7Methods/Ex01SumTwoIntegers.java
@@ -0,0 +1,11 @@
+package Part7Methods;
+
+public class Ex01SumTwoIntegers {
+    static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(5, 3));
+    }
+}
