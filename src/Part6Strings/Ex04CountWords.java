c1237e202ef1852f61705a62b3e3d1bcac8654d0
VPhuong2K6 <nvphuong2k6@gmail.com>
2025-10-22 18:58:37 +0700

    Update Part6 Ex04CountWords

---
+++ src/Part6Strings/Ex04CountWords.java
@@ -0,0 +1,17 @@
+package Part6Strings;
+import java.util.Scanner;
+
+public class Ex04CountWords {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập chuỗi: ");
+        String str = sc.nextLine().trim();
+
+        if (str.isEmpty()) {
+            System.out.println("Số từ: 0");
+        } else {
+            String[] words = str.split("\\s+");
+            System.out.println("Số từ trong chuỗi là: " + words.length);
+        }
+    }
+}
