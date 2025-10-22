d8a5976e8de399ac8a62a544e059bb205bc69109
VPhuong2K6 <nvphuong2k6@gmail.com>
2025-10-22 17:59:37 +0700

    Update Part6 Ex01StringReverse

---
+++ src/Part6Strings/Ex01StringReverse.java
@@ -0,0 +1,13 @@
+package Part6Strings;
+import java.util.Scanner;
+
+public class Ex01StringReverse {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập chuỗi: ");
+        String str = sc.nextLine();
+
+        String reversed = new StringBuilder(str).reverse().toString();
+        System.out.println("Chuỗi đảo ngược là: " + reversed);
+    }
+}
